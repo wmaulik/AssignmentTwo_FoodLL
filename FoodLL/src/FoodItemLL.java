616c450152d66a7296cb5bdae7b053a32da220a3
wmaulik <wm257@email.vccs.edu>
2015-09-30 15:54:07 -0400

    updated FoodItemNode class

--- FoodLL/src/FoodItemLL.java
+++ FoodLL/src/FoodItemLL.java
@@ -1,14 +1,37 @@
 /**
- * 
  * @author Writesh Maulik
- *
  */
-public class FoodItemLL 
-{
 
+import java.io.Serializable;
+
+public class FoodItemLL <X> implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7080974178964077517L;
+	private FoodItemNode <X> head;
+	private FoodItemNode <X> tail;
+	
 	public FoodItemLL() 
 	{
 		// TODO Auto-generated constructor stub
+		this.head = null;	
 	}
-
+	
+	/**
+	 * queues new item to data
+	 */
+	public void queue(X data)
+	{
+		FoodItemNode <X> node = new FoodItemNode<X> (data);
+		if (this.head == null)
+		{
+			this.head = node;
+		}
+		else
+		{
+			this.tail.setPointer(node);
+		}
+	}
 }
--- FoodLL/src/FoodItemNode.java
+++ FoodLL/src/FoodItemNode.java
@@ -3,12 +3,59 @@
  *
  */
 
-public class FoodItemNode 
+import java.io.Serializable;
+
+public class FoodItemNode <T> implements Serializable
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3188182954994050318L;
+	private T data;
+	private FoodItemNode<T> pointer;
 	
-	public FoodItemNode() 
+	public FoodItemNode(T data) 
 	{
 		// TODO Auto-generated constructor stub
+		this.data = data;
+		this.pointer = null;
 	}
-
+	
+	public FoodItemNode(T data, FoodItemNode<T> pointer)
+	{
+		this.data = data;
+		this.pointer = pointer;
+	}
+	
+	/**
+	 * returns data
+	 */
+	public T getData()
+	{
+		return this.data;
+	}
+	
+	/**
+	 * sets data
+	 */
+	public void setData(T data)
+	{
+		this.data = data;
+	}
+	
+	/**
+	 * returns the pointer
+	 */
+	public FoodItemNode<T> getPointer()
+	{
+		return this.pointer;
+	}
+	
+	/**
+	 * sets pointer
+	 */
+	public void setPointer(FoodItemNode<T> pointer)
+	{
+		this.pointer = pointer;
+	}
 }
